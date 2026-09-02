d56f877742ee283f63366c288f98b09ab0b32b09
mor063180-wq <mor063180@gmail.com>
2026-09-03 02:30:47 +0330

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-/usr/sbin/sshd
-cd /usr/local/x-ui && ./x-ui &
-# sing-box فقط وقتی اجرا می‌شه که config.json براش ساخته باشی
-if [ -f /usr/local/sing-box/config.json ]; then
-    cd /usr/local/sing-box && ./sing-box run -c config.json &
-fi
-wait -n
